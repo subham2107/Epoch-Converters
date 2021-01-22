155fbf6ed9b70aae381cc0ef917fa2de57ceeaf8
Subham Mitra <subham.mitra21@gmail.com>
2021-01-22 13:14:43 +0000

    code structure changed

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
 const readLineSync = require('readline-sync');
 
-const user_response = parseInt(readLineSync.question('Welcome to Epoch Converters. Which conversion would you like to use?\n1. Epoch to HumanDate\n2. HumanDate to Epoch\n'));
+const userResponse = parseInt(readLineSync.question('Welcome to Epoch Converters. Which conversion would you like to use?\n1. Epoch to HumanDate\n2. HumanDate to Epoch\n'));
 
-console.log(`You selected ${user_response}`);
-logic(user_response);
+console.log(`You selected ${userResponse}`);
 
 function toHumanDate(epoch)
 {
 
-const epochdate= new Date(epoch);//1611041456000
-const date = epochdate.getDate();
-const month = epochdate.getMonth()+1;
-const year = epochdate.getFullYear();
-const hours = epochdate.getHours();
-const minutes = epochdate.getMinutes();
-const seconds = epochdate.getSeconds(); 
+const epochDate= new Date(epoch);//1611041456000
+const date = epochDate.getDate();
+const month = epochDate.getMonth()+1;
+const year = epochDate.getFullYear();
+const hours = epochDate.getHours();
+const minutes = epochDate.getMinutes();
+const seconds = epochDate.getSeconds(); 
 
-const fullDate={
-  year,month,date,hours,minutes,seconds
-};
+const fullDate={year,month,date,hours,minutes,seconds};
+
 return fullDate;
 }
 
-function toEpoch(humandate)
+function toEpoch(humanDate)
 {
-  const myDate = new Date(humandate); 
+  const myDate = new Date(humanDate); 
   //1 19 2021 07:30:56 → 1611041456000
   const myEpoch = myDate.getTime();
   return myEpoch;
 }
 
-function logic(user_response)
+switch(userResponse)
 {
-if(user_response===1)
-{
+  case 1:
   const epoch = parseInt(readLineSync.question('Please enter the Epoch to be converted to HumanDate\n'));
   console.log(toHumanDate(epoch));
-}
+  break;
 
-else if(user_response===2)
-{
+  case 2:
   console.log('Please enter the HumanDate (year,month,day,hour,minute,second one by one) to be converted to Epoch\n');
 
   const year = readLineSync.question('Please enter the year\n');
@@ -54,11 +50,13 @@ else if(user_response===2)
 
   const second = readLineSync.question('Please enter the second\n');
 
-  const humandate=month+' '+date+' '+year+' '+hour+':'+minute+':'+second;
-  console.log(toEpoch(humandate));
-}
-else
-{
+  const humanDate = `${month} ${date} ${year} ${hour}:${minute}:${second}`;
+
+  console.log(toEpoch(humanDate));
+  break;
+
+  default:
   console.log('Invalid option');
+  break;
 }
-}
+
