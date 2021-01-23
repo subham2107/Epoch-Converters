123a611125734da01595414c6c4f19a54a4adba5
Subham Mitra <subham.mitra21@gmail.com>
2021-01-23 12:35:47 +0000

    improved indentation

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(`You selected ${userResponse}`);
 
 function toHumanDate(epoch)
 {
-   const epochDate= new Date(epoch);//1611041456000
+   const epochDate = new Date(epoch);//1611041456000
    const date = epochDate.getDate();
    const month = epochDate.getMonth()+1;
    const year = epochDate.getFullYear();
@@ -14,7 +14,7 @@ function toHumanDate(epoch)
    const minutes = epochDate.getMinutes();
    const seconds = epochDate.getSeconds(); 
 
-   const fullDate={year,month,date,hours,minutes,seconds};
+   const fullDate = {year,month,date,hours,minutes,seconds};
 
    return fullDate;
 }
@@ -29,32 +29,32 @@ function toEpoch(humanDate)
 switch(userResponse)
 {
    case 1:
-   const epoch = parseInt(readLineSync.question('Please enter the Epoch to be converted to HumanDate\n'));
-   console.log(toHumanDate(epoch));
-   break;
+     const epoch = parseInt(readLineSync.question('Please enter the Epoch to be converted to HumanDate\n'));
+     console.log(toHumanDate(epoch));
+     break;
 
    case 2:
-   console.log('Please enter the HumanDate (year,month,day,hour,minute,second one by one) to be converted to Epoch\n');
+     console.log('Please enter the HumanDate (year,month,day,hour,minute,second one by one) to be converted to Epoch\n');
 
-   const year = readLineSync.question('Please enter the year\n');
+     const year = readLineSync.question('Please enter the year\n');
 
-   const month = readLineSync.question('Please enter the month\n');
+     const month = readLineSync.question('Please enter the month\n');
 
-   const date = readLineSync.question('Please enter the date\n');
+     const date = readLineSync.question('Please enter the date\n');
 
-   const hour = readLineSync.question('Please enter the hour\n');
+     const hour = readLineSync.question('Please enter the hour\n');
 
-   const minute = readLineSync.question('Please enter the minute\n');
+     const minute = readLineSync.question('Please enter the minute\n');
 
-   const second = readLineSync.question('Please enter the second\n');
+     const second = readLineSync.question('Please enter the second\n');
 
-   const humanDate = `${month} ${date} ${year} ${hour}:${minute}:${second}`;
+     const humanDate = `${month} ${date} ${year} ${hour}:${minute}:${second}`;
 
-   console.log(toEpoch(humanDate));
-   break;
+     console.log(toEpoch(humanDate));
+     break;
 
    default:
-   console.log('Invalid option');
-   break;
+     console.log('Invalid option');
+     break;
 }
 
